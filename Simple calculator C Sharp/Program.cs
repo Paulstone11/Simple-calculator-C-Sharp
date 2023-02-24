d4e01d2caee9a83d01676768f706361bd60f17d6
admin <admin@CHAOS>
2023-02-25 00:31:40 +0700

    Add project files.

---
+++ Simple calculator C Sharp/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+
+Console.WriteLine("Enter the first number");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the second number");
+int secondNumber = int.Parse(Console.ReadLine());
+
+int answer = firstNumber + secondNumber;
+
+Console.WriteLine("{0} + {1} = {2}", firstNumber, secondNumber, answer);
